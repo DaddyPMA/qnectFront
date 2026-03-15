8b397363aba1c529bb939ffa592127ad0464e06e
Morgan <morgantaylorh94@outlook.com>
2026-03-15 17:28:25 -0500

    update api url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://qnect-backend.up.railway.app/api';
+const API_URL = 'https://qnectback-production.up.railway.app';
 
 const api = axios.create({
   baseURL: API_URL,
