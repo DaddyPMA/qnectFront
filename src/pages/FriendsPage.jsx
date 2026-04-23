b8034fa36a296b18be8fe3f1d38cd25ab3168520
Morgan <morgantaylorh94@outlook.com>
2026-04-23 10:49:37 -0500

    fix friend css import

--- src/pages/FriendsPage.jsx
+++ src/pages/FriendsPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../api';
-import '../styles/friends.css';
+import "../friends.css";
 
 export default function FriendsPage() {
   const navigate = useNavigate();
